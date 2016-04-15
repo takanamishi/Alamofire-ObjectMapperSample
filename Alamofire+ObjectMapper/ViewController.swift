228e1b5f59ed90f4b7b5d9896e70fabdabe5811c
takanamishi <takanamishi@mac.local>
2016-04-15 22:51:18 +0900

    Initial Commit

---
+++ Alamofire+ObjectMapper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Alamofire+ObjectMapper
+//
+//  Created by takanamishi on 2016/04/15.
+//  Copyright © 2016年 sample. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
